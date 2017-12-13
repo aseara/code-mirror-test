6365a21d1b05e7a648f04824c3fcf35dde1e6c6c
aseara <aseara@163.com>
2017-12-13 11:15:06 +0800

    成功引用code mirror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+
+import {Component} from '@angular/core';
+import 'codemirror/mode/javascript/javascript';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Codemirror sample with Angular 2</h1>
+    <codemirror [(ngModel)]="content" [config]="config"></codemirror>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  config;
+  content;
+
+  constructor() {
+    this.config = { lineNumbers: true, mode: 'text/x-go' };
+    this.content = `// ... some code !
+package main
+
+import "fmt"
+
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i  // Send 'i' to channel 'ch'
+	}
+}`;
+
+  }
 }
